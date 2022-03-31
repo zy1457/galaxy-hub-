724be8425dbad785583f2be5aad5aa176e55c4fe
zy1457 <88177813+zy1457@users.noreply.github.com>
2022-03-31 19:28:06 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,17 @@
 
-
+local url = "https://raw.githubusercontent.com/zy1457/galaxy-hub/main/games"
 
 local games = ({ 
     [155615604] = "Prison Life"
 })
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+
+local name = games[game.PlaceId] or games[game.GameId]
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
+
 
 

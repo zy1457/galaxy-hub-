de952ba40bb8283cc02812f1eb8507e32ef087e5
zy1457 <88177813+zy1457@users.noreply.github.com>
2022-07-03 15:10:08 +0800

    Update Prison_Life.lua

--- games/Prison_Life.lua
+++ games/Prison_Life.lua
@@ -3,4 +3,4 @@ local plr = game.Players.LocalPlayer
 
 local gameid = 155615604
 if game.PlaceId ~= gameid then
-    plr:Disconnect("pls join prison life to use this script")
+    plr:Kick("pls join prison life to use this script")
